a05a15952e026cb5798af4c01a521f900f35f94a
Rui Miguel Alonso <ruiskas@gmail.com>
2020-10-25 15:29:28 +0100

    Build: created basic app

---
+++ app/src/main/java/com/ruiskas/tebeo/TebeoApplication.kt
@@ -0,0 +1,16 @@
+package com.ruiskas.tebeo
+
+import android.app.Application
+
+class TebeoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    private fun initDI() {
+
+    }
+
+}
